fb5a56d41fc5f6f0b6a8760fadb52554792aa79b
Ariel7871 <Ariel7871@gmail.com>
2022-08-11 18:25:53 +0300

    init commit

--- 13.py
+++ 13.py
@@ -5,12 +5,17 @@ def save_name(names_file):
     my_file.close()
 
 
-def show_name(names_file):
+def show_name(names_file, blabla):
     my_file = open(names_file, "r")
     for name in my_file.readlines():
         print(name, end='')
     my_file.close()
 
+
 for i in range(5):
     save_name("names.txt")
 show_name("names.txt")
+
+
+def moshe(david):
+    print(david)
